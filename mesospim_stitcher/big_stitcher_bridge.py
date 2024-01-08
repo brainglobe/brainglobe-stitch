5d1604a3b768e2c25dee362323fbea68da890d90
IgorTatarnikov <igor.tatarnikov@gmail.com>
2024-01-08 16:00:06 +0000

    Select channel on which to stich, options for downsampling factor

--- mesospim_stitcher/big_stitcher_bridge.py
+++ mesospim_stitcher/big_stitcher_bridge.py
@@ -1,26 +1,23 @@
 import subprocess
 from pathlib import Path
 
-XML_PATH = (
-    "D:/TiledDataset/2.5x_tile/"
-    "2.5x_tile_igor_rightonly_Mag2.5x_ch488_ch561_ch647_bdv3.xml"
-)
-TILE_CONFIG_PATH = (
-    "D:/TiledDataset/"
-    "2.5x_tile/"
-    "2.5x_tile_igor_rightonly_Mag2.5x"
-    "_ch488_ch561_ch647_bdv_tile_config.txt"
-)
-
 
 def run_big_stitcher(
-    imageJ_path: Path, xml_path: Path, tile_config_path: Path
+    imageJ_path: Path,
+    xml_path: Path,
+    tile_config_path: Path,
+    all_channels: bool = False,
+    selected_channel: int = 488,
+    downsample_x: int = 4,
+    downsample_y: int = 4,
+    downsample_z: int = 4,
 ):
     stitch_macro_path = Path(__file__).resolve().parent / "stitch_macro.ijm"
     command = (
         f"{imageJ_path} --ij2"
         f" --headless -macro {stitch_macro_path} "
-        f'"{xml_path} {tile_config_path}"'
+        f'"{xml_path} {tile_config_path} {int(all_channels)}'
+        f' {selected_channel} {downsample_x} {downsample_y} {downsample_z}"'
     )
 
     result = subprocess.run(
@@ -28,15 +25,3 @@ def run_big_stitcher(
     )
 
     return result
-
-
-if __name__ == "__main__":
-    result = run_big_stitcher(
-        Path("C:/Users/Igor/Documents/Fiji.app/ImageJ-win64.exe"),
-        Path(XML_PATH),
-        Path(TILE_CONFIG_PATH),
-    )
-
-    with open("big_stitcher_output.txt", "w") as f:
-        f.write(result.stdout)
-        f.write(result.stderr)
