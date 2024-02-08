927f21aa67f4e57df1fa2163c8349a4b803bae8d
IgorTatarnikov <igor.tatarnikov@gmail.com>
2024-02-08 15:51:43 +0000

    Rename to brainglobe-stitch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ autodoc_mock_imports = []
 # use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath("../.."))
 
-project = "mesospim_stitcher"
+project = "brainglobe_stitch"
 copyright = "2023, Igor Tatarnikov"
 author = "Igor Tatarnikov"
 try:
@@ -86,7 +86,7 @@ exclude_patterns = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = "pydata_sphinx_theme"
-html_title = "mesospim_stitcher"
+html_title = "brainglobe_stitch"
 
 # Customize the theme
 html_theme_options = {
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,9 +1,9 @@
-.. mesospim-stitcher documentation master file, created by
+.. brainglboe-stitch documentation master file, created by
    sphinx-quickstart on Fri Dec  9 14:12:42 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to mesospim-stitcher's documentation!
+Welcome to brainglobe-stitch's documentation!
 =========================================================
 
 .. toctree::
