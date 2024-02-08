b0c14a76c9868521534f3aeb191aa911f1f313a4
IgorTatarnikov <igor.tatarnikov@gmail.com>
2024-02-08 17:45:52 +0000

    Added docstring for big_stitcher_bridge:run_big_stitcher

--- brainglobe_stitch/big_stitcher_bridge.py
+++ brainglobe_stitch/big_stitcher_bridge.py
@@ -12,7 +12,40 @@ def run_big_stitcher(
     downsample_x: int = 4,
     downsample_y: int = 4,
     downsample_z: int = 4,
-):
+) -> subprocess.CompletedProcess:
+    """
+    Run the BigStitcher ImageJ macro in headless mode. Output is captured
+    and returned as part of the subprocess.CompletedProcess.
+
+    Parameters
+    ----------
+    imagej_path: Path
+        The path to the ImageJ executable.
+    xml_path: Path
+        The path to the BigDataViewer XML file.
+    tile_config_path: Path
+        The path to the BigStitcher tile configuration file.
+    all_channels: bool
+        Whether to stitch based on all channels.
+    selected_channel: int
+        The channel on which to base the stitching.
+    downsample_x: int
+        The downsample factor in the x-dimension for the stitching.
+    downsample_y: int
+        The downsample factor in the y-dimension for the stitching.
+    downsample_z: int
+        The downsample factor in the z-dimension for the stitching.
+
+    Returns
+    -------
+    subprocess.CompletedProcess
+        The result of the subprocess run.
+
+    Raises
+    ------
+    subprocess.CalledProcessError
+        If the subprocess returns a non-zero exit status.
+    """
     stitch_macro_path = (
         Path(__file__).resolve().parent / "bigstitcher_macro.ijm"
     )
