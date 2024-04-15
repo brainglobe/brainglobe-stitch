a1406843a6409d25e93dab6fdc83931bffe5f765
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2024-04-15 16:22:03 +0100

    Add create_pyramid_bdv_h5 function to create downsample pyramid

--- brainglobe_stitch/file_utils.py
+++ brainglobe_stitch/file_utils.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
+import h5py
+import numpy as np
+
 HEADERS = [
     "[POSITION]",
     "[ETL PARAMETERS]",
@@ -8,77 +11,66 @@ HEADERS = [
 ]
 
 
-def write_big_stitcher_tile_config(meta_file_name: Path) -> list[dict]:
-    tile_metadata = parse_mesospim_metadata(meta_file_name)
+def create_pyramid_bdv_h5(
+    input_file: Path,
+    yield_progress: bool = False,
+):
+    """
+    Create a resolution pyramid for a Big Data Viewer HDF5 file.
 
-    output_file = str(meta_file_name)[:-12] + "_tile_config.txt"
+    Parameters
+    ----------
+    input_file: Path
+        The path to the input HDF5 file.
+    yield_progress: bool, optional
+        Whether to yield progress.
+    """
+    resolutions_array = np.array(
+        [[1, 1, 1], [2, 2, 2], [4, 4, 4], [8, 8, 8], [16, 16, 16]]
+    )
 
-    first_channel = tile_metadata[0]["Laser"]
-    num_channels = 1
+    subdivisions_array = np.array(
+        [[32, 32, 16], [32, 32, 16], [32, 32, 16], [32, 32, 16], [32, 32, 16]]
+    )
 
-    for metadata in tile_metadata[1:]:
-        if metadata["Laser"] == first_channel:
-            break
-        else:
-            num_channels += 1
+    with h5py.File(input_file, "r+") as f:
+        data_group = f["t00000"]
+        num_done = 0
+        num_slices = len(data_group)
 
-    num_tiles = len(tile_metadata) // num_channels
-    tile_xy_locations = []
+        for curr_slice in data_group:
+            # Delete the old resolutions and subdivisions datasets
+            del f[f"{curr_slice}/resolutions"]
+            del f[f"{curr_slice}/subdivisions"]
+            f[f"{curr_slice}/resolutions"] = resolutions_array
+            f[f"{curr_slice}/subdivisions"] = subdivisions_array
 
-    for i in range(0, len(tile_metadata), num_channels):
-        curr_tile_dict = tile_metadata[i]
+            grp: h5py.Group = f[f"t00000/{curr_slice}"]
+            for i in range(1, resolutions_array.shape[0]):
+                downsampling_factors = (
+                    resolutions_array[i] // resolutions_array[i - 1]
+                )
+                prev_resolution = grp[f"{i - 1}/cells"]
+                # Add 1 to account for odd dimensions
+                grp.require_dataset(
+                    f"{i}/cells",
+                    dtype=prev_resolution.dtype,
+                    shape=(
+                        (prev_resolution.shape[0] + 1)
+                        // downsampling_factors[2],
+                        (prev_resolution.shape[1] + 1)
+                        // downsampling_factors[1],
+                        (prev_resolution.shape[2] + 1)
+                        // downsampling_factors[0],
+                    ),
+                )
+                grp[f"{i}/cells"][...] = prev_resolution[
+                    :: downsampling_factors[2],
+                    :: downsampling_factors[1],
+                    :: downsampling_factors[0],
+                ]
 
-        x = round(curr_tile_dict["x_pos"] / curr_tile_dict["Pixelsize in um"])
-        y = round(curr_tile_dict["y_pos"] / curr_tile_dict["Pixelsize in um"])
+            num_done += 1
 
-        tile_xy_locations.append((x, y))
-
-    relative_locations = [(0, 0)]
-
-    for abs_tuple in tile_xy_locations[1:]:
-        rel_tuple = (
-            abs(abs_tuple[0] - tile_xy_locations[0][0]),
-            abs(abs_tuple[1] - tile_xy_locations[0][1]),
-        )
-        relative_locations.append(rel_tuple)
-
-    with open(output_file, "w") as f:
-        f.write("dim=3\n")
-        for i in range(len(tile_metadata)):
-            f.write(
-                f"{i};;"
-                f"({relative_locations[i%num_tiles][0]},"
-                f"{relative_locations[i%num_tiles][1]},0)\n"
-            )
-
-    return tile_metadata
-
-
-def parse_mesospim_metadata(meta_file_name: Path):
-    tile_metadata = []
-    with open(meta_file_name, "r") as f:
-        lines = f.readlines()
-        curr_tile_metadata: dict[str, str | int | float] = {}
-
-        for line in lines[3:]:
-            line = line.strip()
-            if line.startswith("[CFG"):
-                tile_metadata.append(curr_tile_metadata)
-                curr_tile_metadata = {}
-            elif line in HEADERS:
-                continue
-            elif not line:
-                continue
-            else:
-                split_line = line.split("]")
-                value = split_line[1].strip()
-                if value.isdigit():
-                    curr_tile_metadata[split_line[0][1:]] = int(value)
-                else:
-                    try:
-                        curr_tile_metadata[split_line[0][1:]] = float(value)
-                    except ValueError:
-                        curr_tile_metadata[split_line[0][1:]] = value
-
-    tile_metadata.append(curr_tile_metadata)
-    return tile_metadata
+            if yield_progress:
+                yield int(100 * num_done / num_slices)
