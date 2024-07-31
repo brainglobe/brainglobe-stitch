faed1bf4b8a43e9a1d64043663a2ae50fa3e96ae
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2024-07-31 15:41:15 +0100

    Added comments for conftest.py

--- tests/test_unit/conftest.py
+++ tests/test_unit/conftest.py
@@ -11,6 +11,15 @@ TEST_DATA_URL = "https://gin.g-node.org/IgorTatarnikov/brainglobe-stitch-test/ra
 
 @pytest.fixture(scope="session", autouse=True)
 def download_test_data():
+    """
+    Downloads the test data and extracts it to a temporary directory.
+    This fixture is session-scoped and automatically run once.
+
+    Yields
+    ------
+    Path
+        The path to the temporary directory.
+    """
     TEMP_DIR.mkdir(exist_ok=True)
     pooch.retrieve(
         TEST_DATA_URL,
@@ -25,11 +34,30 @@ def download_test_data():
 
 @pytest.fixture(scope="session")
 def test_data_directory():
+    """
+    Returns the path to the clean test data directory.
+
+    Yields
+    ------
+    Path
+        The path to the clean test data directory.
+    """
     yield TEMP_DIR
 
 
 @pytest.fixture(scope="module")
 def naive_bdv_directory():
+    """
+    Creates a temporary directory and copies the test data to it. This allows
+    tests to modify the directory without affecting the original test data.
+
+    The temporary directory is cleaned up after the tests are run.
+
+    Yields
+    ------
+    Path
+        The path to the temporary directory.
+    """
     test_dir = Path.home() / "test_directory"
 
     shutil.copytree(
@@ -50,6 +78,19 @@ def naive_bdv_directory():
 
 @pytest.fixture
 def bdv_directory_function_level():
+    """
+    Creates a temporary directory and copies the test data to it. This allows
+    tests to modify the directory without affecting the original test data.
+
+    This fixture is function-scoped.
+
+    The temporary directory is cleaned up after the tests are run.
+
+    Yields
+    ------
+    Path
+        The path to the temporary directory.
+    """
     test_dir = Path.home() / "quick_test_directory"
 
     shutil.copytree(
@@ -64,8 +105,18 @@ def bdv_directory_function_level():
     shutil.rmtree(test_dir)
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="session")
 def imagej_path():
+    """
+    Returns the path to a mock ImageJ executable based on the operating
+    system. This is used to mimic the behavior of the QFileDialog in macOS,
+    it returns a path to the "Fiji.app" directory instead of the executable.
+
+    Returns
+    -------
+    Path
+        The path to the mock ImageJ executable.
+    """
     if system() == "Windows":
         return Path.home() / "Fiji.app/ImageJ-win64.exe"
     elif system() == "Darwin":
@@ -76,21 +127,36 @@ def imagej_path():
 
 @pytest.fixture(scope="module")
 def test_constants(imagej_path):
-    # The tiles lie in one z-plane and are arranged in a 2x2 grid.
-    # There are 2 channels.
-    # Each tile is 128x128x107 pixels (x, y, z).
-    # The tiles overlap by 10% in x and y (13 pixels).
-    # The tiles are arranged in the following pattern:
-    # channel 0   | channel 1
-    # 00 10          | 04 05
-    # 01 11           | 14 15
+    """
+    Provides a dictionary of constants that's used in the tests.
+    Contains metadata about the test data and the expected results.
 
-    # EXPECTED_TILE_CONFIG is based on test_data_bdv.xml
-    # The tile positions are in pixels in x, y, z order
+    The tiles lie in one z-plane and are arranged in a 2x2 grid.
+    There are 2 channels.
+    Each tile is 128x128x107 pixels (x, y, z).
+    The tiles overlap by 10% in x and y (13 pixels).
+    The tiles are arranged in the following pattern:
+    channel 0   | channel 1
+     00 10          | 04 05
+     01 11           | 14 15
 
-    # EXPECTED_TILE_POSITIONS are based on the stitch transforms found
-    # in test_data_bdv.xml
-    # The tile positions are in pixels in z, y, x order
+    EXPECTED_TILE_CONFIG is based on test_data_bdv.xml
+    The tile positions are in pixels in x, y, z order
+
+    EXPECTED_TILE_POSITIONS are based on the stitch transforms found
+    in test_data_bdv.xml
+    The tile positions are in pixels in z, y, x order
+
+    Parameters
+    ----------
+    imagej_path : Path
+        The path to the mock ImageJ executable.
+
+    Returns
+    -------
+    Dict
+        A dictionary containing the constants.
+    """
     constants_dict = {
         "NUM_TILES": 8,
         "NUM_CHANNELS": 2,
