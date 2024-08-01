8302bac2ca5a55f1746090d793b4fb509cbb2fad
Igor Tatarnikov <igor.tatarnikov@gmail.com>
2024-08-01 11:06:47 +0100

    Add docstring to test_file_utils

--- tests/test_unit/test_file_utils.py
+++ tests/test_unit/test_file_utils.py
@@ -15,7 +15,17 @@ from brainglobe_stitch.file_utils import (
 
 
 @pytest.fixture
-def bad_bdv_directory():
+def invalid_bdv_directory():
+    """
+    Fixture for creating an invalid directory for testing.
+    The directory is created but empty. The directory is deleted after each
+    test.
+
+    Yields
+    ------
+    Path
+        The invalid directory for testing.
+    """
     bad_dir = Path("./bad_directory")
     bad_dir.mkdir()
 
@@ -27,6 +37,15 @@ def bad_bdv_directory():
 def test_create_pyramid_bdv_h5(
     naive_bdv_directory, test_data_directory, test_constants
 ):
+    """
+    Check the create_pyramid_bdv_h5 function. The function should create a
+    resolution pyramid of depth 5 for each tile in the h5 file. The function
+    modifies the h5 file in place. The results are checked by comparing the
+    modified h5 file to the expected h5 file, which is stored in the
+    test_data_directory.
+    """
+    # Sanity check to ensure that the test h5 file doesn't contain any
+    # resolutions or subdivisions, and no resolution pyramid.
     h5_path = naive_bdv_directory / "test_data_bdv.h5"
     with h5py.File(h5_path, "r") as f:
         num_tiles = len(f["t00000"].keys())
@@ -37,6 +56,8 @@ def test_create_pyramid_bdv_h5(
             assert f[f"{tile_name}/subdivisions"].shape[0] == 1
             assert len(f[f"t00000/{tile_name}"].keys()) == 1
 
+    # Run the function and check that the correct value is yielded for each
+    # iteration (percent complete)
     num_done = 1
     for progress in create_pyramid_bdv_h5(h5_path, yield_progress=True):
         assert progress == int(100 * num_done / num_tiles)
@@ -52,6 +73,9 @@ def test_create_pyramid_bdv_h5(
 
         tile_names = f_in["t00000"].keys()
 
+        # Check that the resolutions and subdivisions have been added for
+        # each tile, and that the resolution pyramid of correct depth has been
+        # created for each tile.
         for tile_name in tile_names:
             assert (
                 f_out[f"{tile_name}/resolutions"].shape[0]
@@ -68,11 +92,18 @@ def test_create_pyramid_bdv_h5(
 
 
 def test_parse_mesospim_metadata(naive_bdv_directory, test_constants):
+    """
+    Check the parse_mesospim_metadata function. The function should parse the
+    metadata stored in the h5_meta.txt file and return a list of dictionaries,
+    one for each tile. The results are checked by comparing the metadata to the
+    expected metadata, which is stored in the test_constants dictionary.
+    """
     meta_path = naive_bdv_directory / "test_data_bdv.h5_meta.txt"
 
     meta_data = parse_mesospim_metadata(meta_path)
 
     assert len(meta_data) == test_constants["NUM_TILES"]
+    # The tiles are alternating in channel names
     for i in range(test_constants["NUM_TILES"]):
         assert meta_data[i]["Laser"] == test_constants["CHANNELS"][i % 2]
         assert (
@@ -109,13 +140,17 @@ def test_check_mesospim_directory(naive_bdv_directory):
     ],
 )
 def test_check_mesospim_directory_missing_files(
-    bad_bdv_directory, file_names, error_message
+    invalid_bdv_directory, file_names, error_message
 ):
+    """
+    Add the specified files to the invalid directory and check that the
+    FileNotFoundError is raised with the correct error message.
+    """
     for file_name in file_names:
-        Path(bad_bdv_directory / file_name).touch()
+        Path(invalid_bdv_directory / file_name).touch()
 
     with pytest.raises(FileNotFoundError) as e:
-        check_mesospim_directory(bad_bdv_directory)
+        check_mesospim_directory(invalid_bdv_directory)
 
         assert error_message in str(e)
 
@@ -143,13 +178,17 @@ def test_check_mesospim_directory_missing_files(
     ],
 )
 def test_check_mesospim_directory_too_many_files(
-    bad_bdv_directory, file_names, error_message
+    invalid_bdv_directory, file_names, error_message
 ):
+    """
+    Add the specified files to the invalid directory and check that the
+    FileNotFoundError is raised with the correct error message.
+    """
     for file_name in file_names:
-        Path(bad_bdv_directory / file_name).touch()
+        Path(invalid_bdv_directory / file_name).touch()
 
     with pytest.raises(FileNotFoundError) as e:
-        check_mesospim_directory(bad_bdv_directory)
+        check_mesospim_directory(invalid_bdv_directory)
 
         assert error_message in str(e)
 
