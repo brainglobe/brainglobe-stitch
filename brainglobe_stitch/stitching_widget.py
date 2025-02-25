a5169c908160af61fdff5d7ab541c89c5aae5985
Adam Tyson <code@adamltyson.com>
2025-02-25 14:57:59 +0000

    Import Qt elements from qt-niu (#30)

--- brainglobe_stitch/stitching_widget.py
+++ brainglobe_stitch/stitching_widget.py
@@ -8,11 +8,11 @@ import h5py
 import napari
 import numpy as np
 import numpy.typing as npt
-from brainglobe_utils.qtpy.dialog import display_info, display_warning
 from brainglobe_utils.qtpy.logo import header_widget
 from napari import Viewer
 from napari.qt.threading import create_worker
 from napari.utils.notifications import show_info, show_warning
+from qt_niu.dialog import display_info, display_warning
 from qtpy.QtWidgets import (
     QComboBox,
     QFileDialog,
